7642c727c2e75e99b5a63b1efb4e5d647316cd6c
Christopher Neal <wandadars@gmail.com>
2018-03-16 13:29:25 -0400

    pep8 for readability of flowpanel.py

--- interfaces/cython/cantera/mixmaster/flowpanel.py
+++ interfaces/cython/cantera/mixmaster/flowpanel.py
@@ -3,44 +3,40 @@
 
 # functionality imports
 import sys
+from Cantera.gui import menu, newflow
+
 if sys.version_info[0] == 3:
     from tkinter import *
 else:
     from Tkinter import *
 
-from Cantera.gui import menu, newflow
 
 class App:
     def __init__(self, master):
-
         try:
             self.root = master.root
         except:
             self.root = master
 
         self.frame = Frame(master)
-        self.frame.grid(row = 0, column = 0)
-
+        self.frame.grid(row=0, column=0)
         self.makemenu(self.frame)
 
-        self.quitbutton = Button(self.frame, text = "Quit",
-                                 command = self.frame.quit)
-        self.quitbutton.grid(row = 1, column = 0)
+        self.quitbutton = Button(self.frame, text='Quit', command=self.frame.quit)
+        self.quitbutton.grid(row=1, column=0)
 
-        self.newbutton = Button(self.frame, text = "New...",
-                                command = self.notyet)
-        self.newbutton.grid(row = 1, column = 1)
+        self.newbutton = Button(self.frame, text='New...', command=self.notyet)
+        self.newbutton.grid(row=1, column=1)
 
     def notyet(self):
         print('not yet!')
 
-
     def newflow(self):
         n = newflow.NewFlowDialog(self.root)
 
-    def makemenu(self,frame):
+    def makemenu(self, frame):
         self.menubar = Frame(frame, relief=FLAT, bd=0)
-        self.menubar.grid(row = 0, column = 0)
+        self.menubar.grid(row=0, column=0)
 
         self.filemenu = menu.make_menu('File', self.menubar,
                                   [('New...', self.newflow),
@@ -54,7 +50,5 @@ class App:
 
 
 root = Tk()
-
 app = App(root)
-
 root.mainloop()
