c7646ec2c7d861b53b76c9f62d71b9fdb5181b5d
Christopher Neal <wandadars@gmail.com>
2018-03-16 13:38:24 -0400

    pep8 for readability of ImportFrame.py

--- interfaces/cython/cantera/mixmaster/ImportFrame.py
+++ interfaces/cython/cantera/mixmaster/ImportFrame.py
@@ -1,7 +1,11 @@
 # This file is part of Cantera. See License.txt in the top-level directory or
 # at http://www.cantera.org/license.txt for license and copyright information.
 
-import os, math,sys
+import os
+import math
+import sys
+from cantera import *
+#from Cantera.ck2ctml import ck2ctml
 
 if sys.version_info[0] == 3:
     from tkinter import *
@@ -10,48 +14,40 @@ else:
     from Tkinter import *
     from tkFileDialog import askopenfilename
 
-from cantera import *
-#from Cantera.ck2ctml import ck2ctml
 
 class ImportFrame(Frame):
-    def __init__(self,top):
+    def __init__(self, top):
         self.master = Toplevel()
         self.master.title('Convert and Import CK File')
-        self.master.protocol("WM_DELETE_WINDOW",self.hide)
+        self.master.protocol("WM_DELETE_WINDOW", self.hide)
 
-        Frame.__init__(self,self.master)
+        Frame.__init__(self, self.master)
         self.config(relief=GROOVE, bd=4)
         self.top = top
         self.infile = StringVar()
 
-        Label(self,text="Input File").grid(row=0,column=0)
-        Entry(self, width=40,
-              textvariable=self.infile).grid(column=1,row=0)
-        Button(self, text='Browse',
-               command=self.browseForInput).grid(row=0,column=2)
+        Label(self, text="Input File").grid(row=0, column=0)
+        Entry(self, width=40, textvariable=self.infile).grid(column=1, row=0)
+        Button(self, text='Browse', command=self.browseForInput).grid(row=0, column=2)
 
         self.thermo = StringVar()
-        Label(self,text="Thermodynamic Database").grid(row=1,column=0)
-        Entry(self, width=40,
-              textvariable=self.thermo).grid(column=1,row=1)
-        Button(self, text='Browse',
-               command=self.browseForThermo).grid(row=1,column=2)
+        Label(self, text="Thermodynamic Database").grid(row=1, column=0)
+        Entry(self, width=40, textvariable=self.thermo).grid(column=1, row=1)
+        Button(self, text='Browse', command=self.browseForThermo).grid(row=1, column=2)
 
 
         self.transport = StringVar()
-        Label(self,text="Transport Database").grid(row=2,column=0)
-        Entry(self, width=40,
-              textvariable=self.transport).grid(column=1,row=2)
-        Button(self, text='Browse',
-               command=self.browseForTransport).grid(row=2,column=2)
+        Label(self, text="Transport Database").grid(row=2, column=0)
+        Entry(self, width=40, textvariable=self.transport).grid(column=1, row=2)
+        Button(self, text='Browse', command=self.browseForTransport).grid(row=2, column=2)
 
         bframe = Frame(self)
         bframe.config(relief=GROOVE, bd=1)
-        bframe.grid(row=100,column=0)
-        Button(bframe, text='OK', width=8, command=self.importfile).grid(row=0,column=0)
-        self.grid(column=0,row=0)
-        Button(bframe, text='Cancel', width=8, command=self.hide).grid(row=0,column=1)
-        self.grid(column=0,row=0)
+        bframe.grid(row=100, column=0)
+        Button(bframe, text='OK', width=8, command=self.importfile).grid(row=0, column=0)
+        self.grid(column=0, row=0)
+        Button(bframe, text='Cancel', width=8, command=self.hide).grid(row=0, column=1)
+        self.grid(column=0, row=0)
         self.hide()
 
     def browseForInput(self, e=None):
@@ -89,9 +85,11 @@ class ImportFrame(Frame):
         fname = os.path.basename(ckfile)
         ff = os.path.splitext(fname)
         nm = ""
-        if len(ff) > 1: nm = ff[0]
-        else: nm = ff
-        outfile = p+os.sep+nm+'.xml'
+        if len(ff) > 1:
+            nm = ff[0]
+        else:
+            nm = ff
+        outfile = p+os.sep + nm + '.xml'
         try:
             print('not supported.')
             #ck2ctml(infile = ckfile, thermo = thermdb,
@@ -105,7 +103,7 @@ class ImportFrame(Frame):
             self.hide()
             return
 
-        self.top.loadmech(nm,outfile,1)
+        self.top.loadmech(nm, outfile, 1)
         self.hide()
 
 ##              cmd = 'ck2ctml -i '+ckfile+' -o '+outfile
@@ -117,7 +115,6 @@ class ImportFrame(Frame):
 ##              ok = os.system(cmd)
 ##              if ok == 0:
 ##                      self.top.loadmech(nm,outfile,1)
-
 
     def hide(self):
         #self.vis.set(0)
