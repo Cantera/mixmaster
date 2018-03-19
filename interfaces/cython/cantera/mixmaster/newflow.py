4e351eae01e1c42cd66d988df38eca0dc551499b
Christopher Neal <wandadars@gmail.com>
2018-03-19 16:31:07 -0400

    pep8 standardization of tkinter module import for newflow.py

--- interfaces/cython/cantera/mixmaster/newflow.py
+++ interfaces/cython/cantera/mixmaster/newflow.py
@@ -2,81 +2,81 @@
 # at http://www.cantera.org/license.txt for license and copyright information.
 
 import sys
-from cantera import *
+import cantera as ct
 
 if sys.version_info[0] == 3:
-    from tkinter import *
+    import tkinter as tk
     from tkinter.filedialog import askopenfilename
     from tkinter import messagebox
 else:
-    from Tkinter import *
+    import Tkinter as tk
     import tkMessageBox as messagebox
     from tkFileDialog import askopenfilename
 
 
 class NewFlowDialog:
     def __init__(self, parent):
-        top = self.top = Toplevel(parent)
+        top = self.top = tk.Toplevel(parent)
 
-        pl = Label(top, text='Pressure')
+        pl = tk.Label(top, text='Pressure')
         pl.grid(row=0, column=0)
 
-        geom = Frame(top, bd=2, relief=GROOVE)
+        geom = tk.Frame(top, bd=2, relief=tk.GROOVE)
         geom.grid(row=1, column=0)
-        lb = Listbox(geom)
-        for item in ["One-Dimensional", "Stagnation"]:
-            lb.insert(END, item)
+        lb = tk.Listbox(geom)
+        for item in ['One-Dimensional', 'Stagnation']:
+            lb.insert(tk.END, item)
         lb.grid(row=0, column=0)
-        glb = Listbox(geom)
-        for item in ["Axisymmetric", "2D"]:
-            glb.insert(END, item)
+        glb = tk.Listbox(geom)
+        for item in ['Axisymmetric', '2D']:
+            glb.insert(tk.END, item)
         glb.grid(row=1, column=0)
 
         # ------------- pressure input ----------------
-        self.p = DoubleVar()
-        self.pbox = Entry(top, textvariable=self.p)
+        self.p = tk.DoubleVar()
+        self.pbox = tk.Entry(top, textvariable=self.p)
         self.pbox.grid(row=0, column=1)
 
         # ------------- gas file name input -----------
-        gasf = Frame(top, bd=2, relief=GROOVE)
+        gasf = tk.Frame(top, bd=2, relief=tk.GROOVE)
         gasf.grid(row=4, column=0, columnspan=2)
-        gl = Label(gasf, text='Gas Mixture Specification')
+        gl = tk.Label(gasf, text='Gas Mixture Specification')
         gl.grid(row=0, column=0)
 
-        self.infile = StringVar()
-        Label(gasf, text='Mixture Input File').grid(row=1, column=0)
-        Entry(gasf, textvariable=self.infile).grid(row=1, column=1)
-        Button(gasf, text='Browse..', command=self.getinfile).grid(row=1, column=2)
+        self.infile = tk.StringVar()
+        tk.Label(gasf, text='Mixture Input File').grid(row=1, column=0)
+        tk.Entry(gasf, textvariable=self.infile).grid(row=1, column=1)
+        tk.Button(gasf, text='Browse..', command=self.getinfile).grid(row=1, column=2)
 
-        self.spfile = StringVar()
-        Label(gasf, text='Species Database').grid(row=2, column=0)
-        Entry(gasf, textvariable=self.spfile).grid(row=2, column=1)
-        Button(gasf, text='Browse..', command=self.getspfile).grid(row=2, column=2)
+        self.spfile = tk.StringVar()
+        tk.Label(gasf, text='Species Database').grid(row=2, column=0)
+        tk.Entry(gasf, textvariable=self.spfile).grid(row=2, column=1)
+        tk.Button(gasf, text='Browse..', command=self.getspfile).grid(row=2, column=2)
 
-        self.trfile = StringVar()
-        Label(gasf, text='Transport Database').grid(row=3, column=0)
-        Entry(gasf, textvariable=self.trfile).grid(row=3, column=1)
-        Button(gasf, text='Browse..', command=self.gettrfile).grid(row=3, column=2)
+        self.trfile = tk.StringVar()
+        tk.Label(gasf, text='Transport Database').grid(row=3, column=0)
+        tk.Entry(gasf, textvariable=self.trfile).grid(row=3, column=1)
+        tk.Button(gasf, text='Browse..', command=self.gettrfile).grid(row=3, column=2)
 
         # ------------- grid -------------------------
-        gf = Frame(top, bd=2, relief=GROOVE)
+        gf = tk.Frame(top, bd=2, relief=tk.GROOVE)
         gf.grid(row=5, column=0, columnspan=2)
 
-        gr = Label(gf, text='Initial Grid')
+        gr = tk.Label(gf, text='Initial Grid')
         gr.grid(row=0, column=0)
 
-        self.zleft = DoubleVar()
-        self.zright = DoubleVar()
-        ll = Label(gf, text='Left boundary at ')
-        rl = Label(gf, text='Right boundary at ')
-        lbb = Entry(gf, textvariable=self.zleft)
-        rbb = Entry(gf, textvariable=self.zright)
+        self.zleft = tk.DoubleVar()
+        self.zright = tk.DoubleVar()
+        ll = tk.Label(gf, text='Left boundary at ')
+        rl = tk.Label(gf, text='Right boundary at ')
+        lbb = tk.Entry(gf, textvariable=self.zleft)
+        rbb = tk.Entry(gf, textvariable=self.zright)
         ll.grid(row=1, column=0)
         rl.grid(row=2, column=0)
         lbb.grid(row=1, column=1)
         rbb.grid(row=2, column=1)
 
-        ok = Button(top, text = 'OK', command=self.ok)
+        ok = tk.Button(top, text='OK', command=self.ok)
         ok.grid(row=20, column=20)
 
     def ok(self):
@@ -86,14 +86,14 @@ class NewFlowDialog:
             spfile = self.spfile.get()
             trfile = self.trfile.get()
             if spfile and trfile:
-                self.gas = IdealGasMix(import_file=infile,
-                                       thermo_db=spfile,
-                                       transport_db=trfile)
+                self.gas = ct.IdealGasMix(import_file=infile,
+                                          thermo_db=spfile,
+                                          transport_db=trfile)
             elif spfile:
-                self.gas = IdealGasMix(import_file=infile,
-                                       thermo_db=spfile)
+                self.gas = ct.IdealGasMix(import_file=infile,
+                                          thermo_db=spfile)
             else:
-                self.gas = IdealGasMix(import_file=infile)
+                self.gas = ct.IdealGasMix(import_file=infile)
 
         except:
             messagebox.showerror('Create Gas',
