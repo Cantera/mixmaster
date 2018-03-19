47d3b6e5ceb08685ccec47f865f501e93cd8e705
Christopher Neal <wandadars@gmail.com>
2018-03-19 12:35:46 -0400

    pep8 for readability of UnitChooser.py

--- interfaces/cython/cantera/mixmaster/UnitChooser.py
+++ interfaces/cython/cantera/mixmaster/UnitChooser.py
@@ -1,17 +1,18 @@
 # This file is part of Cantera. See License.txt in the top-level directory or
 # at http://www.cantera.org/license.txt for license and copyright information.
 
+import re
 import sys
+
 if sys.version_info[0] == 3:
     from tkinter import *
 else:
     from Tkinter import *
 
-import re
 
 class UnitVar(Frame):
-    def __init__(self,master,unitmod,defaultunit=0):
-        Frame.__init__(self,master)
+    def __init__(self, master, unitmod, defaultunit=0):
+        Frame.__init__(self, master)
         self.x = DoubleVar()
         self.xsi = 0.0
         self.x.set(0.0)
@@ -24,18 +25,18 @@ class UnitVar(Frame):
             for it in unitlist:
                 if it[0] != '_':
                     self.unitlist.append(it)
-        self.v = Entry(self,textvariable=self.x)
+        self.v = Entry(self, textvariable=self.x)
         self.s = StringVar()
-        tmp = re.sub('__',' / ',self.unitlist[defaultunit])
+        tmp = re.sub('__', ' / ', self.unitlist[defaultunit])
         self.s.set(tmp)
-        self.conv = eval('self.unitmod.'+re.sub(' / ','__',self.s.get())).value
+        self.conv = eval('self.unitmod.' + re.sub(' / ', '__', self.s.get())).value
         self.u = Label(self)
-        self.u.config(textvariable=self.s,fg='darkblue')
+        self.u.config(textvariable=self.s, fg='darkblue')
         self.u.bind('<Double-1>', self.select)
-        self.u.bind('<Any-Enter>',self.highlight)
-        self.u.bind('<Any-Leave>',self.nohighlight)
-        self.v.grid(row=0,column=0)
-        self.u.grid(row=0,column=1)
+        self.u.bind('<Any-Enter>', self.highlight)
+        self.u.bind('<Any-Leave>', self.nohighlight)
+        self.v.grid(row=0, column=0)
+        self.u.grid(row=0, column=1)
 
     def highlight(self, event=None):
         self.u.config(fg='yellow')
@@ -47,44 +48,42 @@ class UnitVar(Frame):
         self.new=Toplevel()
         self.new.title("Units")
         self.new.transient(self.master)
-        self.new.bind("<Return>", self.finished,"+")
+        self.new.bind("<Return>", self.finished, "+")
 
-        r=0
-        c=0
+        r = 0
+        c = 0
         for each in self.unitlist:
             if each[0] != '_' and each[:1] != '__' and each != 'SI':
-                each = re.sub('__',' / ',each)
-                Radiobutton(self.new,
-                            text=each,
-                            variable=self.u['textvariable'],
+                each = re.sub('__', ' / ', each)
+                Radiobutton(self.new, text=each, variable=self.u['textvariable'],
                             value=each,
                             command=self.update,
                             ).grid(column=c, row=r, sticky=W)
                 r += 1
-                if (r>10):
-                    r=0
+                if (r > 10):
+                    r = 0
                     c += 1
                     r += 1
 
-        b=Button(self.new,text='OK',command=self.finished, default=ACTIVE)
+        b=Button(self.new, text='OK', command=self.finished, default=ACTIVE)
         b.grid(column=c, row=r)
 
         self.new.grab_set()
         self.new.focus_set()
         self.new.wait_window()
 
-    def finished(self,event=None):
+    def finished(self, event=None):
         self.new.destroy()
 
     def update(self):
         self.xsi = self.x.get() * self.conv
-        self.conv = eval('self.unitmod.'+re.sub(' / ','__',self.s.get())).value
-        self.x.set(self.xsi/self.conv)
+        self.conv = eval('self.unitmod.' + re.sub(' / ', '__', self.s.get())).value
+        self.x.set(self.xsi / self.conv)
 
     def get(self):
         self.xsi = self.x.get() * self.conv
         return self.xsi
 
-    def set(self,value):
+    def set(self, value):
         self.xsi = value
-        self.x.set(value/self.conv)
+        self.x.set(value / self.conv)
