286bb7804807a96667885c8f3d77213239a8f0f3
Christopher Neal <wandadars@gmail.com>
2018-03-19 17:20:48 -0400

    pep8 standardization of tkinter module import for TransportFrame.py

--- interfaces/cython/cantera/mixmaster/TransportFrame.py
+++ interfaces/cython/cantera/mixmaster/TransportFrame.py
@@ -4,15 +4,15 @@
 import sys
 
 if sys.version_info[0] == 3:
-    from tkinter import *
+    import tkinter as tk
 else:
-    from Tkinter import *
+    import Tkinter as tk
 
 
-class TransportFrame(Frame):
+class TransportFrame(tk.Frame):
     def show(self, i, frame, row, col):
         if self.checked[i].get():
-            frame.grid(row=row, column=col, sticky=N+E+S+W)
+            frame.grid(row=row, column=col, sticky=tk.N + tk.E + tk.S + tk.W)
         else:
             frame.grid_forget()
 
@@ -26,13 +26,13 @@ class TransportFrame(Frame):
         self.top = top
         self.c = []
         self.checked = []
-        Frame.__init__(self, master)
-        self.config(relief=GROOVE, bd=4)
+        tk.Frame.__init__(self, master)
+        self.config(relief=tk.GROOVE, bd=4)
         lbl = ['multicomponent', 'mixture-averaged']
         cmds = [self.showcomp, self.showthermo]
         for i in range(2):
-            self.checked.append(IntVar())
+            self.checked.append(tk.IntVar())
             self.checked[i].set(0)
-            self.c.append(Checkbutton(self, text=lbl[i], variable=self.checked[i],
-                                      onvalue=1, offvalue=0, command=cmds[i]))
-            self.c[i].grid(column=i, row=0, sticky=W+N)
+            self.c.append(tk.Checkbutton(self, text=lbl[i], variable=self.checked[i],
+                                         onvalue=1, offvalue=0, command=cmds[i]))
+            self.c[i].grid(column=i, row=0, sticky=tk.W + tk.N)
