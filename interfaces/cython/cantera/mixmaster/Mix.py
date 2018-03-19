fc35b337a6e3ae0a78a9508481857aea4bfb1d75
Christopher Neal <wandadars@gmail.com>
2018-03-19 11:49:18 -0400

    pep8 for readability of Mix.py

--- interfaces/cython/cantera/mixmaster/Mix.py
+++ interfaces/cython/cantera/mixmaster/Mix.py
@@ -1,9 +1,10 @@
 # This file is part of Cantera. See License.txt in the top-level directory or
 # at http://www.cantera.org/license.txt for license and copyright information.
 
-from cantera import gas_constant
 from numpy import zeros, ones
+from cantera import gas_constant
 from .utilities import handleError
+
 
 def spdict(phase, x):
     nm = phase.speciesNames()
@@ -12,8 +13,9 @@ def spdict(phase, x):
         data[nm[k]] = x[k]
     return data
 
+
 class Species:
-    def __init__(self,g,name):
+    def __init__(self, g, name):
         self.g = g
         t = g.T
         p = g.P
@@ -26,34 +28,35 @@ class Species:
         self.molecularWeight = g.molecular_weights[self.index]
         self.c = []
         self.e = g.element_names
-        self.hf0 = self.enthalpy_RT(298.15)*gas_constant*298.15
-        g.TPX = t,p,x
+        self.hf0 = self.enthalpy_RT(298.15) * gas_constant * 298.15
+        g.TPX = t, p, x
         for n in range(len(self.e)):
             na = g.n_atoms(self.index, n)
             if na > 0:
-                self.c.append((self.e[n],na))
+                self.c.append((self.e[n], na))
 
     def composition(self):
         return self.c
 
-    def enthalpy_RT(self,t):
+    def enthalpy_RT(self, t):
         self.g.TP = t, None
-        return self.g.partial_molar_enthalpies[self.index] / (gas_constant*t)
+        return self.g.partial_molar_enthalpies[self.index] / (gas_constant * t)
 
-    def cp_R(self,t):
+    def cp_R(self, t):
         self.g.TP = t, None
         return self.g.standard_cp_R[self.index]
 
-    def entropy_R(self,t):
+    def entropy_R(self, t):
         self.g.TP = t, None
         return self.g.standard_entropies_R[self.index]
 
+
 class Mix:
-    def __init__(self,g):
+    def __init__(self, g):
         self.g = g
         self._mech = g
         self.nsp = g.n_species
-        self._moles = zeros(self.nsp,'d')
+        self._moles = zeros(self.nsp, 'd')
         self.wt = g.molecular_weights
 
     def setMoles(self, m):
@@ -72,7 +75,7 @@ class Mix:
     def totalMass(self):
         sum = 0.0
         for k in range(self.nsp):
-            sum += self._moles[k]*self.wt[k]
+            sum += self._moles[k] * self.wt[k]
         return sum
 
     def moleDict(self):
@@ -83,10 +86,10 @@ class Mix:
         return d
 
     def setMass(self, m):
-        self.setMoles( m/self.wt)
+        self.setMoles(m / self.wt)
 
     def mass(self):
-        return self.wt*self._moles
+        return self.wt * self._moles
 
     def speciesNames(self):
         return self.g.species_names
@@ -95,12 +98,11 @@ class Mix:
         d = {}
         nm = self.g.species_names
         for e in range(self.nsp):
-            d[nm[e]] = self._moles[e]*self.wt[e]
+            d[nm[e]] = self._moles[e] * self.wt[e]
         return d
 
-    def set(self, temperature = None, pressure = None,
-            density = None, enthalpy = None,
-            entropy = None, intEnergy = None, equil = 0):
+    def set(self, temperature=None, pressure=None, density=None, enthalpy=None,
+            entropy=None, intEnergy=None, equil=0):
         total_mass = self.totalMass()
 
         if temperature and pressure:
@@ -124,12 +126,12 @@ class Mix:
                 self.g.equilibrate('SP')
 
         elif density and entropy:
-            self.g.SV = entropy, 1.0/density
+            self.g.SV = entropy, 1.0 / density
             if equil:
                 self.g.equilibrate('SV')
 
         elif density and intEnergy:
-            self.g.UV = intEnergy, 1.0/density
+            self.g.UV = intEnergy, 1.0 / density
             if equil:
                 self.g.equilibrate('UV')
 
@@ -137,5 +139,5 @@ class Mix:
 #               handleError('unsupported property pair', warning=1)
 
 
-        total_moles = total_mass/self.g.mean_molecular_weight
-        self._moles = self.g.X*total_moles
+        total_moles = total_mass / self.g.mean_molecular_weight
+        self._moles = self.g.X * total_moles
